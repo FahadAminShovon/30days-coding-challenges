10f44a2c872ba9556ee4d2f04bd14a8700a770f9
Fahad Amin <bm.amin.fahad@gamil.com>
2018-12-29 18:03:10 +0600

    Create Day 03.py

---
+++ Python/Day 03.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def solve(n):
+    if (n%2)==1:
+        print("Weird")
+    else:
+        if (n>=2 and n<=5):print("Not Weird")
+        elif (n<=20):print("Weird")
+        else: print("Not Weird")
+        
+
+if __name__ == '__main__':
+    N = int(input())
+    solve(N)
