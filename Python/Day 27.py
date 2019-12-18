a3db481a9cb9a6b78f5615a186e250535a609483
FahadAminShovon <bm.amin.fahad@gmail.com>
2019-12-18 07:26:46 +0600

    add Day 27.py

---
+++ Python/Day 27.py
@@ -0,0 +1,32 @@
+
+class TestDataEmptyArray(object):
+    
+    @staticmethod
+    def get_array():
+        # complete this function
+        return []
+
+class TestDataUniqueValues(object):
+
+    @staticmethod
+    def get_array():
+        # complete this 
+        return [1,2,3,4,5,6,8,0]
+
+    @staticmethod
+    def get_expected_result():
+        # complete this function
+        return 7
+
+class TestDataExactlyTwoDifferentMinimums(object):
+
+    @staticmethod
+    def get_array():
+        # complete this function
+        return [0,1,2,3,1,0]
+
+    @staticmethod
+    def get_expected_result():
+        # complete this function
+        return 0
+
