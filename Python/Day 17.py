270e115f6092461febff7d20517875159a031332
FahadAminShovon <bm.amin.fahad@gmail.com>
2019-12-18 07:19:10 +0600

    add Day 17.py

---
+++ Python/Day 17.py
@@ -0,0 +1,9 @@
+#Write your code here
+
+class Calculator(Exception):
+    def power(self,n,p):
+        if(n<0 or p<0):
+            raise Exception('n and p should be non-negative')
+        else:
+            return n**p
+
